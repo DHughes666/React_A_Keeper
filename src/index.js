7723db4552920ab0126c82cd56c3ffabcabb11ad
DHughes666 <graziemolto6@gmail.com>
2023-08-19 09:34:06 +0100

    Keeper App initiated

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App'
+ 
+ReactDOM.render(
+  <h1>Hello Worldy</h1>, document.getElementById('root')
+);
