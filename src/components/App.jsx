1a28a9d782d50572c2ba47d4ee7239e8ece3d11c
DHughes666 <graziemolto6@gmail.com>
2023-08-20 16:26:49 +0100

    Expanded Keeper-App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,22 @@ import React from "react";
 import Header from './Header.jsx';
 import Note from './Note.jsx';
 import Footer from './Footer.jsx';
+import notes from "../notes.js";
+
+function createNote(noteItem){
+    return (
+        <Note 
+            title={noteItem.title} 
+            content = {noteItem.content} 
+        />
+    )
+}
 
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(createNote)}
         <Footer />
     </div>
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,10 +3,10 @@ import React from "react";
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 
-function Note(){
+function Note(props){
     return<div className="note">
-        <h1>Keep the Diary</h1>
-        <p>Here is a notebook for your thought.</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 }
 
