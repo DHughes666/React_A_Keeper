3bda8d18af9473c5e61fc200958467f75d7329e2
DHughes666 <graziemolto6@gmail.com>
2023-08-20 02:45:38 +0100

    Modified Index.js to fit latest React Version

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-//1. Create a new React app.
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import App from "./components/App.jsx";
 
-import App from './components/App.jsx'
-//2. Create a App.jsx component.
-//3. Create a Header.jsx component that renders a <header> element
-//to show the Keeper App name in an <h1>.
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
-//5. Create a Note.jsx component to show a <div> element with a
-//<h1> for a title and a <p> for the content.
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
-
-ReactDOM.render(
-  <App />, document.getElementById('root'),
-)
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
